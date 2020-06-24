666659f8c559e38f78e2d98aec94911d5e70363a
Han-Seulgi <skhan20@naver.com>
2020-06-25 03:54:41 +0900

    일지 화면 쫌쫌따리

--- app/src/main/java/com/hsk/mycatdiary/ui/diary/DiaryFragment.java
+++ app/src/main/java/com/hsk/mycatdiary/ui/diary/DiaryFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,7 +16,6 @@ public class DiaryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_diary, container, false);
-        final TextView textView = root.findViewById(R.id.text_diary);
         return root;
     }
 }
--- app/src/main/java/com/hsk/mycatdiary/ui/home/HomeFragment.java
+++ app/src/main/java/com/hsk/mycatdiary/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,11 +12,21 @@ import androidx.fragment.app.Fragment;
 import com.hsk.mycatdiary.R;
 
 public class HomeFragment extends Fragment {
+    ImageButton btnShortcut;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        btnShortcut = root.findViewById(R.id.btnCheck);
+
+        btnShortcut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return root;
     }
